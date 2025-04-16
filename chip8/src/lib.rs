ce8d019c407d33398c8c2c3b479545ee5d4a925b
aidengage <aidenfgage@gmail.com>
2025-04-16 15:46:51 -0400

    added op_add function from guide, also added few constants for screen and memory sizes

---
+++ chip8/src/lib.rs
@@ -0,0 +1,36 @@
+use rand::rngs::SmallRng;
+use rand::{RngCore, SeedableRng};
+
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+const MEM_SIZE: usize = 0x1000;
+const ROM_ADDR: usize = 0x200;
+
+pub struct Chip8<R: RngCore> {
+    mem: [u8; MEM_SIZE],
+    v: Regs,
+    i: u16,
+    pc: u16,
+    stack: [u16; 0x10],
+    sp: u8,
+    dt: u8,
+    st: u8,
+    keypad: u16,
+    fb: [u8; SCREEN_WIDTH * SCREEN_HEIGHT / 8],
+    tone: bool,
+    time: isize,
+    rng: R,
+}
+
+/// Op: Set Vx = Vx + b.
+fn op_add(&mut self, x: Reg, b: u8) -> usize {
+    let (res, overflow) = self.v[x].overflowing_add(b);
+    self.v[x] = res;
+    self.v[Reg(0xf)] = if overflow { 1 } else { 0 };
+    self.pc += 2;
+    45
+}
+
+fn main() {
+    println!("Hello, world!");
+}
